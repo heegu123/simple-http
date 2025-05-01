f65c60626679f8a58524215fbc99f9d9fd7f54bb
heegu123 <kookminkhg@kookmin.ac.kr>
2025-05-01 22:35:30 +0900

    add client.util

---
+++ server/src/main/java/myhttp/server/util/JsonUtil.java
@@ -0,0 +1,25 @@
+// server/src/main/java/myhttp/server/util/JsonUtil.java
+package myhttp.server.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtil {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static String toJson(Object obj) {
+        try {
+            return mapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("JSON serialization error", e);
+        }
+    }
+
+    public static <T> T fromJson(String json, Class<T> clazz) {
+        try {
+            return mapper.readValue(json, clazz);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("JSON deserialization error", e);
+        }
+    }
+}
